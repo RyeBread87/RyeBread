d5bb1b57ddb35ee0f9099206a0b74b0ee63bffa4
TheRyanKing87 <smithry87@gmail.com>
2020-07-25 21:27:50 +0100

    Initial commit

---
+++ src/JustBlog/JustBlog/Models/SiteStatsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RyeBread.Models
+{
+    public class SiteStatsModel
+    {
+        public int TotalPosts { get; set; }
+
+        public int TotalCategories { get; set; }
+    }
+}
---
+++ src/JustBlog/JustBlog/Models/WidgetViewModel.cs
@@ -0,0 +1,33 @@
+﻿using RyeBread.Core;
+using RyeBread.Core.Objects;
+using System.Collections.Generic;
+
+namespace RyeBread.Models
+{
+  /// <summary>
+  /// View model used to wrap data for sidebar widgets.
+  /// </summary>
+  public class WidgetViewModel
+  {
+    public WidgetViewModel(IBlogRepository blogRepository)
+    {
+      Categories = blogRepository.Categories();
+      siteStatsModel = new SiteStatsModel();
+      //Tags = blogRepository.Tags();
+      LatestPosts = blogRepository.Posts(0, 10);
+      siteStatsModel.TotalPosts = blogRepository.TotalPosts(true);
+      siteStatsModel.TotalCategories = blogRepository.TotalCategories();
+    }
+
+    public IList<Category> Categories 
+    { get; private set; }
+
+    public IList<Tag> Tags 
+    { get; private set; }
+
+    public IList<Post> LatestPosts 
+    { get; private set; }
+    
+    public SiteStatsModel siteStatsModel { get; set; }
+    }
+}
